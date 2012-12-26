65babf9c0626659c6aaf860b00fda54841f947ae
oscmvs <support@oscmvs.com>
2012-12-25 17:53:24 -0800

    First Commit

---
+++ catalog/includes/configure.php
@@ -0,0 +1,31 @@
+<?php
+  define('HTTP_SERVER', 'http://www.oscmvs.com');
+  define('HTTPS_SERVER', 'http://www.oscmvs.com');
+  define('ENABLE_SSL', false);
+  define('HTTP_COOKIE_DOMAIN', '');
+  define('HTTPS_COOKIE_DOMAIN', '');
+  define('HTTP_COOKIE_PATH', '/labs/oscmvs-2.3.3/catalog/');
+  define('HTTPS_COOKIE_PATH', '/labs/oscmvs-2.3.3/catalog/');
+  define('DIR_WS_HTTP_CATALOG', '/labs/oscmvs-2.3.3/catalog/');
+  define('DIR_WS_HTTPS_CATALOG', '/labs/oscmvs-2.3.3/catalog/');
+  define('DIR_WS_IMAGES', 'images/');
+  define('DIR_WS_ICONS', DIR_WS_IMAGES . 'icons/');
+  define('DIR_WS_INCLUDES', 'includes/');
+  define('DIR_WS_FUNCTIONS', DIR_WS_INCLUDES . 'functions/');
+  define('DIR_WS_CLASSES', DIR_WS_INCLUDES . 'classes/');
+  define('DIR_WS_MODULES', DIR_WS_INCLUDES . 'modules/');
+  define('DIR_WS_LANGUAGES', DIR_WS_INCLUDES . 'languages/');
+
+  define('DIR_WS_DOWNLOAD_PUBLIC', 'pub/');
+  define('DIR_FS_CATALOG', '/home/ecomme33/public_html/labs/oscmvs-2.3.3/catalog/');
+  define('DIR_FS_DOWNLOAD', DIR_FS_CATALOG . 'download/');
+  define('DIR_FS_DOWNLOAD_PUBLIC', DIR_FS_CATALOG . 'pub/');
+
+  define('DB_SERVER', 'localhost');
+  define('DB_SERVER_USERNAME', 'ecomme33_mvs233');
+  define('DB_SERVER_PASSWORD', 'lasvegas.2040');
+  define('DB_DATABASE', 'ecomme33_mvs233');
+  define('USE_PCONNECT', 'false');
+  define('STORE_SESSIONS', 'mysql');
+  define('CFG_TIME_ZONE', 'America/Tijuana');
+?>
